12921775ef88d308a411a86967ec6e84f8ecb687
AhmadFayyadhFadhil <fadhilahmad1199@gmail.com>
2024-02-29 21:36:30 +0700

    pesan commit

--- admin/updateuser.php
+++ admin/updateuser.php
@@ -2,12 +2,12 @@
 include '../koneksi.php';
 
 if(isset($_POST['update'])) {
-    $id_user = $_POST['nama'];
+    $userid = $_POST['userid'];
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Lakukan proses update data di database
-    $query = "UPDATE user SET username='$username', password='$password' WHERE nama=$nama";
+    
+    $query = "UPDATE user SET username='$username', password='$password' WHERE userid=$userid";
     $result = mysqli_query($mysqli, $query);
 
     if($result) {
@@ -21,16 +21,10 @@ if(isset($_POST['update'])) {
 
 // Mendapatkan data user yang akan diupdate
 if(isset($_GET['id'])) {
-    $nama = $_GET['id'];
-    $query = "SELECT * FROM user WHERE id_user=$nama";
+    $userid = $_GET['id'];
+    $query = "SELECT * FROM user WHERE userid=$userid";
     $result = mysqli_query($mysqli, $query);
-
-    if($result && mysqli_num_rows($result) > 0) {
-        $data = mysqli_fetch_assoc($result);
-    } else {
-        echo "Data user tidak ditemukan.";
-        exit();
-    }
+    $data = mysqli_fetch_assoc($result);
 } else {
     echo "ID user tidak ditemukan.";
     exit();
@@ -40,4 +34,35 @@ if(isset($_GET['id'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update User</title>
+    <link rel="icon" type="image/png" href="img/logotitle.png">
+    <link rel="stylesheet" href="styleupdate.css">
+</head>
+<body>
+<div class="container">
+    <header>
+        <h1 class="title">Update User</h1>
+    </header>
+    <section class="form">
+        <form method="POST" action="">
+            <input type="hidden" name="userid" value="<?php echo $data['userid']; ?>">
+            
+            
+            <label for="nama">Nama:</label><br>
+            <input type="text" id="nama" name="nama" value="<?php echo $data['nama']; ?>" readonly><br>
+
+            
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" value="<?php echo $data['username']; ?>"><br>
+
+            
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" value="<?php echo $data['password']; ?>"><br>
+            
+            <input type="submit" name="update" value="Update" class="button">
+        </form>
+    </section>
+</div>
+</body>
+</html>
